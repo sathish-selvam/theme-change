b2ab71e13d6a40c47c79dc0a54919b2828246d6f
Sathish Selvam <sathishselvamdev@gmail.com>
2021-12-22 02:51:12 +0530

    added event listener

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const root = document.querySelector(":root");
+const themeToggle = document.querySelector("#theme-toggle");
+const themeProxiyLabel = document.querySelector("#theme-toggle-proxiy");
+const body = document.querySelector("body");
 
-const themeButton = document.querySelector("#toggle-theme");
+themeDefault();
 
-themeButton.addEventListener("click", () => {
-  root.style.setProperty("--secondary-color", "#22577E");
+themeToggle.addEventListener("click", () => {
+  localStorage.setItem("theme", themeToggle.checked ? "light" : "dark");
+  themeDefault();
 });
+
+function themeDefault() {
+  const moon = themeProxiyLabel.querySelector("i.fa-moon");
+  const sun = themeProxiyLabel.querySelector("i.fa-sun");
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    body.classList.add("dark");
+    moon.style.cssText = `visibility:hidden; opacity : 0; `;
+    sun.style.cssText = `visibility:visible; opacity : 1; `;
+  } else {
+    body.classList.remove("dark");
+    sun.style.cssText = `visibility:hidden; opacity : 0; `;
+    moon.style.cssText = `visibility:visible; opacity : 1; `;
+  }
+}
+
+/*
+  [] Cover image dark move conversion using Figma
+  [] Toggle used the checked box to showcase we can implement custome checkbox also checking state of html (checked attribute) to know the status
+  [] Added smooth transition to all the elment
+
+
+
+  [] logo wrapper for dark mode
+  [] animation 
+  [] responsive design
+*/
